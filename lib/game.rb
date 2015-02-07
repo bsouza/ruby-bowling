bdb043b1a9163245d3c0a214468848f8eb835403
Bruno Souza <to.bsouza@gmail.com>
2015-02-07 19:01:56 -0200

    added Game method to get specific Frame score

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,30 @@ class Game
 
   def initialize
     @score = 0
+    @throws = []
+    @current_throw = 0
   end
 
   def add(pins)
+    @current_throw += 1
+    @throws[@current_throw] = pins
     @score += pins
+  end
+
+  def score_for_frame(the_frame)
+    ball = 1
+    score = 0
+
+    the_frame.times do
+      first_throw = @throws[ball]
+
+      ball += 1
+      second_throw = @throws[ball]
+
+      score += first_throw + second_throw
+    end
+
+    score
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,7 +8,36 @@ RSpec.describe "Game" do
 
   it "should have scored 5 points after knock down 5 pins" do
     @game.add(5)
-    expect(@game.score).to equal(5)
+    game_score_should_be 5
   end
+
+  it "should have scored 9 points after one throw of 5 pins and other of 4 pins" do
+    do_throws([5, 4])
+    game_score_should_be 9
+  end
+
+  it "should add points of 4 throws when have no marks" do
+    do_throws([5, 4, 9, 6])
+    game_score_should_be 24
+  end
+
+  it "should be able to get score from specific Frame" do
+    do_throws([5, 4])
+    expect_frame(1).to equal(9)
+  end
+
+  private
+
+    def do_throws(throws)
+      throws.each { |throw| @game.add(throw) }
+    end
+
+    def expect_frame(frame)
+      expect(@game.score_for_frame(frame))
+    end
+
+    def game_score_should_be(expected_score)
+      expect(@game.score).to equal(expected_score)
+    end
 
 end
