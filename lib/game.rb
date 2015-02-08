4635f33292e86db9dfa67c0fc17b877a46fbf605
Bruno Souza <to.bsouza@gmail.com>
2015-02-08 12:43:27 -0200

    added ability to handle strikes

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,7 @@ class Game
     @throws[@current_throw] = pins
     @score += pins
 
-    adjust_current_frame
+    adjust_current_frame pins
   end
 
   def score_for_frame(the_frame)
@@ -26,14 +26,18 @@ class Game
       ball += 1
       first_throw = @throws[ball]
 
-      ball += 1
-      second_throw = @throws[ball]
+      if first_throw.equal? 10
+        score += 10 + @throws[ball + 1] + @throws[ball + 2]
+      else
+        ball += 1
+        second_throw = @throws[ball]
 
-      frame_score = first_throw + second_throw
-      score += frame_score
+        frame_score = first_throw + second_throw
+        score += frame_score
 
-      if (frame_score == 10)
-        score += @throws[ball + 1]
+        if (frame_score == 10)
+          score += @throws[ball + 1]
+        end
       end
     end
 
@@ -46,13 +50,19 @@ class Game
 
 private
 
-  def adjust_current_frame
+  def adjust_current_frame(pins)
     if @is_first_throw
-      @is_first_throw = false
+      if pins.equal? 10
+        @current_frame += 1
+      else
+        @is_first_throw = false
+      end
     else
       @is_first_throw = true
       @current_frame += 1
     end
+
+    @current_frame = 11 if @current_frame > 11
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -24,10 +24,24 @@ RSpec.describe "Game" do
     current_frame_should_be 2
   end
 
-  it "should be able to handle spare points" do
-    do_throws 3, 7, 3, 2
+  it "should add points from next throw in spare frame" do
+    do_throws 3, 7, 3, 2, 4
     first_frame_should_be 13
+    second_frame_should_be 18
     game_score_should_be 18
+  end
+
+  it "should add points from next two throws in strike frame" do
+    do_throws 10, 3, 6
+    first_frame_should_be 19
+    game_score_should_be 28
+    current_frame_should_be 3
+  end
+
+  it "should reach 300 points in a perfect game" do
+    12.times { @game.add 10 }
+    game_score_should_be 300
+    current_frame_should_be 11
   end
 
   private
@@ -42,6 +56,10 @@ RSpec.describe "Game" do
 
     def first_frame_should_be(expected_score)
       expect_frame(1).to equal(expected_score)
+    end
+
+    def second_frame_should_be(expected_score)
+      expect_frame(2).to equal(expected_score)
     end
 
     def game_score_should_be(expected_score)
